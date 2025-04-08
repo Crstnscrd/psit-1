1a44d849e5afd31e73eec7643c9bdff7a1bcb588
Crstnscrd <145946887+Crstnscrd@users.noreply.github.com>
2025-04-08 16:20:31 +0800

    Add files via upload
    ACTIVITY 4

---
+++ psit-1-main/about.php
@@ -0,0 +1,18 @@
+ <!-- About section -->
+ <section id="about">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <h3>Lorem, ipsum</h3>
+                    <h2>Lorem ipsum dolor sit amet</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat soluta porro, magnam debitis
+                        aliquid cupiditate autem quibusdam iusto libero voluptatibus a quo maxime hic doloribus
+                        exercitationem deserunt eveniet deleniti quos!</p>
+                    <a href="#" class="btn btn-custom">Explore More</a>
+                </div>
+                <div class="col-lg-6">
+                    <img src="ski.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
